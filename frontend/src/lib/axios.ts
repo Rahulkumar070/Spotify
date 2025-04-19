74dd7006c692e05162c754be41f908bd3f5cb6af
Rahul kumar Pal <151990777+Rahulkumar070@users.noreply.github.com>
2025-04-19 09:49:23 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api/songs/trending"
-      : "https://spotify-backend-ky5s.onrender.com/api/songs/trending",
+      : "https://spotify-backend-ky5s.onrender.com/api",
   withCredentials: true,
 });
