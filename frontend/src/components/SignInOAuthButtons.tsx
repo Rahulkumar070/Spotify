412dd06f99f3d0fbf0e1bdc934a64efd8f6c7b5b
Rahul kumar Pal <151990777+Rahulkumar070@users.noreply.github.com>
2025-04-19 10:13:37 +0530

    Update SignInOAuthButtons.tsx

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -1,43 +1,26 @@
 import { useSignIn } from "@clerk/clerk-react";
 import { Button } from "./ui/button";
 
-const providers = [
-  {
-    name: "Google",
-    icon: "/google.png",
-    strategy: "oauth_google",
-  },
-  // Add more providers here in the future
-];
-
 const SignInOAuthButtons = () => {
-  const { signIn, isLoaded } = useSignIn();
+	const { signIn, isLoaded } = useSignIn();
 
-  if (!isLoaded) return null;
+	if (!isLoaded) {
+		return null;
+	}
 
-  const handleOAuthSignIn = (strategy) => {
-    signIn.authenticateWithRedirect({
-      strategy,
-      redirectUrl: "/sso-callback",
-      redirectUrlComplete: "/auth-callback",
-    });
-  };
+	const signInWithGoogle = () => {
+		signIn.authenticateWithRedirect({
+			strategy: "oauth_google",
+			redirectUrl: "/sso-callback",
+			redirectUrlComplete: "/auth-callback",
+		});
+	};
 
-  return (
-    <div className="space-y-2">
-      {providers.map(({ name, icon, strategy }) => (
-        <Button
-          key={strategy}
-          onClick={() => handleOAuthSignIn(strategy)}
-          variant="secondary"
-          className="w-full text-white border-zinc-200 h-11 flex items-center gap-3"
-        >
-          <img src={icon} alt={`${name} logo`} className="size-5" />
-          Continue with {name}
-        </Button>
-      ))}
-    </div>
-  );
+	return (
+		<Button onClick={signInWithGoogle} variant={"secondary"} className='w-full text-white border-zinc-200 h-11'>
+			<img src='/google.png' alt='Google' className='size-5' />
+			Continue with Google
+		</Button>
+	);
 };
-
 export default SignInOAuthButtons;
