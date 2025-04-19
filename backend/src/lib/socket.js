b61755aee89a060c708595be8d563229247ffa1a
Rahul kumar Pal <151990777+Rahulkumar070@users.noreply.github.com>
2025-04-19 09:19:23 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -4,7 +4,11 @@ import { Message } from "../models/message.model.js";
 export const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: [
+        "http://localhost:3000", // dev
+        "https://spotify-frontend-pkyg.onrender.com", // deployed frontend
+      ],
+      methods: ["GET", "POST"],
       credentials: true,
     },
   });
