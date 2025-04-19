4054e09d597fc8d2a633faaa3d8da55d192a2bef
Rahul kumar Pal <151990777+Rahulkumar070@users.noreply.github.com>
2025-04-19 09:04:02 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,5 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:4000/api"
-      : "/api",
+      : "https://spotify-backend-ky5s.onrender.com",
+  withCredentials: true,
 });
