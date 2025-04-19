ced088b7cde3a20ff3ad782b203a4c5bbcc3c89a
Rahul Pal <rk035199@gmail..com>
2025-04-19 07:52:03 +0530

    Complete

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -23,7 +23,7 @@ interface ChatStore {
 }
 
 const baseURL =
-  import.meta.env.MODE === "development" ? "http://localhost:4000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 const socket = io(baseURL, {
   autoConnect: false,
